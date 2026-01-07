6ebe9b0bf11cbffbddfa5eb4e450b393a41c3dc1
Hipe Gelo <roland@hipe.asia>
2026-01-07 13:27:43 +0800

    refactor: improved sound controller

--- lib/controllers/sound_controller.dart
+++ lib/controllers/sound_controller.dart
@@ -6,45 +6,46 @@ class SoundController extends GetxController {
       ? Get.find() 
       : Get.put(SoundController());
 
-  final AudioPlayer _player = AudioPlayer();
+  final AudioPlayer _uiPlayer = AudioPlayer();
+  final AudioPlayer _gamePlayer = AudioPlayer();
 
   Future<void> playClick() async {
-    await _player.stop(); 
-    await _player.play(AssetSource('audio/click.wav')); 
+    if (_uiPlayer.state == PlayerState.playing) await _uiPlayer.stop();
+    await _uiPlayer.play(AssetSource('audio/click.wav')); 
   }
 
   Future<void> playCancel() async {
-    await _player.stop(); 
-    await _player.play(AssetSource('audio/cancel.wav')); 
+    if (_uiPlayer.state == PlayerState.playing) await _uiPlayer.stop();
+    await _uiPlayer.play(AssetSource('audio/cancel.wav')); 
   }
 
   Future<void> playMove() async {
-    await _player.stop();
-    await _player.play(AssetSource('audio/move.mp3'));
+    await _gamePlayer.stop();
+    await _gamePlayer.play(AssetSource('audio/move.mp3'));
   }
 
   Future<void> playOpponentMove() async {
-    await _player.stop();
-    await _player.play(AssetSource('audio/move.mp3'));
+    await _gamePlayer.stop();
+    await _gamePlayer.play(AssetSource('audio/move.mp3'));
   }
 
   Future<void> playCapture() async {
-    await _player.stop();
-    await _player.play(AssetSource('audio/capture.mp3'));
+    await _gamePlayer.stop();
+    await _gamePlayer.play(AssetSource('audio/capture.mp3'));
   }
   
   Future<void> playCheck() async {
-    await _player.stop();
-    await _player.play(AssetSource('audio/check.mp3'));
+    await _gamePlayer.stop();
+    await _gamePlayer.play(AssetSource('audio/check.mp3'));
   }
 
   Future<void> playGameStart() async {
-    await _player.stop();
-    await _player.play(AssetSource('audio/start.mp3'));
+    await _gamePlayer.stop();
+    await _gamePlayer.play(AssetSource('audio/start.mp3'));
   }
 
   Future<void> playGameEnd() async {
-    await _player.stop();
-    await _player.play(AssetSource('audio/end.mp3'));
+    await _gamePlayer.stop();
+    await _gamePlayer.play(AssetSource('audio/end.mp3'));
   }
 }
